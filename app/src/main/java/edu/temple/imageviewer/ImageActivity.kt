e9483571a2d0de9b725cbfdce4796e429ce5e88b
Cecily Devaney <tul06320@temple.edu>
2021-09-22 15:42:21 -0400

    Updated from MainActivity to ImageActivity

--- app/src/main/java/edu/temple/imageviewer/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageviewer/ImageActivity.kt
@@ -3,7 +3,7 @@ package edu.temple.imageviewer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
