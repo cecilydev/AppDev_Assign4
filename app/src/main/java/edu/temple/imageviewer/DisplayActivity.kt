78c657eda0dcaa50ac5742d9fb4f39cf2003c9eb
Cecily Devaney <tul06320@temple.edu>
2021-10-04 16:05:32 -0400

    Started project from assign 3 as base

---
+++ app/src/main/java/edu/temple/imageviewer/DisplayActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.imageviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+    }
+}
--- app/src/main/java/edu/temple/imageviewer/SelectionActivity.kt
+++ app/src/main/java/edu/temple/imageviewer/SelectionActivity.kt
@@ -10,7 +10,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageActivity : AppCompatActivity() {
+class SelectionActivity : AppCompatActivity() {
 
     val data = setData();
 
